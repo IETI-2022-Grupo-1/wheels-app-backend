11453a73a7b5ab126ea41a270577cf4f04c1ebf6
Julian Peña <julian.pena-m@mail.escuelaing.edu.co>
2022-09-13 22:14:07 -0500

    Delete unnecesary imports

--- src/main/java/com/wheelsapp/WheelsappApplication.java
+++ src/main/java/com/wheelsapp/WheelsappApplication.java
@@ -2,10 +2,7 @@ package com.wheelsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import sun.util.calendar.BaseCalendar;
 
-import javax.xml.crypto.Data;
-import java.util.Date;
 
 @SpringBootApplication
 public class WheelsappApplication {
