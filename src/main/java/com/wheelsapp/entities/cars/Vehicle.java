23898ecf709932a7550925487c764c50ebbe135e
Your Name <you@example.com>
2022-09-13 22:15:30 -0500

    Update app.properties and dependencies

--- src/main/java/com/wheelsapp/entities/cars/Vehicle.java
+++ src/main/java/com/wheelsapp/entities/cars/Vehicle.java
@@ -1,10 +1,8 @@
 package com.wheelsapp.entities.cars;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Collections;
 import java.util.Date;
 
 @Document(collection ="cars")
