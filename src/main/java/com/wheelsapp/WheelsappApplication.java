18355ed43fad6c1e1532ab1ce7867eed82f7d65e
Julian Peña <julian.pena-m@mail.escuelaing.edu.co>
2022-09-13 22:38:16 -0500

    Update aplication properties

--- src/main/java/com/wheelsapp/WheelsappApplication.java
+++ src/main/java/com/wheelsapp/WheelsappApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WheelsappApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(WheelsappApplication.class, args);
 	}
 
