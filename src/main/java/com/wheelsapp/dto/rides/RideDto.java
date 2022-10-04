e3895838f2819dd4c5fdb5cc1a4c7c865d73ddca
Juan Cadavid <jua1000n@gmail.com>
2022-10-04 08:56:55 -0500

    Add Lombok

--- src/main/java/com/wheelsapp/dto/rides/RideDto.java
+++ src/main/java/com/wheelsapp/dto/rides/RideDto.java
@@ -1,5 +1,10 @@
 package com.wheelsapp.dto.rides;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -7,6 +12,10 @@ import java.util.HashMap;
 /*
 * @author Julian Peña
 */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RideDto {
     private String id;
     private String idDriver;
@@ -22,135 +31,4 @@ public class RideDto {
     private Integer code;
     private ArrayList<String> passengerList;
     private HashMap<String, String> stopsList;
-
-    public RideDto(){}
-
-    public RideDto(String id, String idDriver, String idCar, Date journeyDate, Date departureHour, Date arrivalHour, ArrayList<String> route, Integer availableSeats, Integer seatsReserved, Boolean isActive, Boolean hasStarted, Integer code, ArrayList<String> passengerList, HashMap<String, String> stopsList) {
-        this.id = id;
-        this.idDriver = idDriver;
-        this.idCar = idCar;
-        this.journeyDate = journeyDate;
-        this.departureHour = departureHour;
-        this.arrivalHour = arrivalHour;
-        this.route = route;
-        this.availableSeats = availableSeats;
-        this.seatsReserved = seatsReserved;
-        this.isActive = isActive;
-        this.hasStarted = hasStarted;
-        this.code = code;
-        this.passengerList = passengerList;
-        this.stopsList = stopsList;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public String getIdDriver() {
-        return idDriver;
-    }
-
-    public void setIdDriver(String idDriver) {
-        this.idDriver = idDriver;
-    }
-
-    public String getIdCar() {
-        return idCar;
-    }
-
-    public void setIdCar(String idCar) {
-        this.idCar = idCar;
-    }
-
-    public Date getJourneyDate() {
-        return journeyDate;
-    }
-
-    public void setJourneyDate(Date journeyDate) {
-        this.journeyDate = journeyDate;
-    }
-
-    public Date getDepartureHour() {
-        return departureHour;
-    }
-
-    public void setDepartureHour(Date departureHour) {
-        this.departureHour = departureHour;
-    }
-
-    public Date getArrivalHour() {
-        return arrivalHour;
-    }
-
-    public void setArrivalHour(Date arrivalHour) {
-        this.arrivalHour = arrivalHour;
-    }
-
-    public ArrayList<String> getRoute() {
-        return route;
-    }
-
-    public void setRoute(ArrayList<String> route) {
-        this.route = route;
-    }
-
-    public Integer getAvailableSeats() {
-        return availableSeats;
-    }
-
-    public void setAvailableSeats(Integer availableSeats) {
-        this.availableSeats = availableSeats;
-    }
-
-    public Integer getSeatsReserved() {
-        return seatsReserved;
-    }
-
-    public void setSeatsReserved(Integer seatsReserved) {
-        this.seatsReserved = seatsReserved;
-    }
-
-    public Boolean getIsActive() {
-        return isActive;
-    }
-
-    public void setIsActive(Boolean isActive) {
-        this.isActive = isActive;
-    }
-
-    public Boolean getHasStarted() {
-        return hasStarted;
-    }
-
-    public void setHasStarted(Boolean hasStarted) {
-        this.hasStarted = hasStarted;
-    }
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public ArrayList<String> getPassengerList() {
-        return passengerList;
-    }
-
-    public void setPassengerList(ArrayList<String> passengerList) {
-        this.passengerList = passengerList;
-    }
-
-    public HashMap<String, String> getStopsList() {
-        return stopsList;
-    }
-
-    public void setStopsList(HashMap<String, String> stopsList) {
-        this.stopsList = stopsList;
-    }
 }
--- src/main/java/com/wheelsapp/entities/rides/Ride.java
+++ src/main/java/com/wheelsapp/entities/rides/Ride.java
@@ -1,5 +1,8 @@
 package com.wheelsapp.entities.rides;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import com.wheelsapp.dto.rides.RideDto;
@@ -7,6 +10,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Document(collection = "Rides")
 public class Ride {
     @Id
@@ -24,8 +30,6 @@ public class Ride {
     private Integer code;
     private ArrayList<String> passengerList;
     private HashMap<String, String> stopsList;
-
-    public Ride(){}
 
     public Ride(RideDto rideDTO) {
         this.idDriver = rideDTO.getIdDriver();
@@ -56,119 +60,5 @@ public class Ride {
         hasStarted = ride.getHasStarted();
         passengerList = ride.getPassengerList();
         stopsList = ride.getStopsList();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getIdDriver() {
-        return idDriver;
-    }
-
-    public void setIdDriver(String idDriver) {
-        this.idDriver = idDriver;
-    }
-
-    public String getIdCar() {
-        return idCar;
-    }
-
-    public void setIdCar(String idCar) {
-        this.idCar = idCar;
-    }
-
-    public Date getJourneyDate() {
-        return journeyDate;
-    }
-
-    public void setJourneyDate(Date journeyDate) {
-        this.journeyDate = journeyDate;
-    }
-
-    public Date getDepartureHour() {
-        return departureHour;
-    }
-
-    public void setDepartureHour(Date departureHour) {
-        this.departureHour = departureHour;
-    }
-
-    public Date getArrivalHour() {
-        return arrivalHour;
-    }
-
-    public void setArrivalHour(Date arrivalHour) {
-        this.arrivalHour = arrivalHour;
-    }
-
-    public ArrayList<String> getRoute() {
-        return route;
-    }
-
-    public void setRoute(ArrayList<String> route) {
-        this.route = route;
-    }
-
-    public Integer getAvailableSeats() {
-        return availableSeats;
-    }
-
-    public void setAvailableSeats(Integer availableSeats) {
-        this.availableSeats = availableSeats;
-    }
-
-    public Integer getSeatsReserved() {
-        return seatsReserved;
-    }
-
-    public void setSeatsReserved(Integer seatsReserved) {
-        this.seatsReserved = seatsReserved;
-    }
-
-    public Boolean getIsActive() {
-        return isActive;
-    }
-
-    public void setIsActive(Boolean isActive) {this.isActive = isActive;}
-
-    public Boolean getHasStarted() {
-        return hasStarted;
-    }
-
-    public void setHasStarted(Boolean hasStarted) {
-        this.hasStarted = hasStarted;
-    }
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public ArrayList<String> getPassengerList() {
-        return passengerList;
-    }
-
-    public void setPassengerList(ArrayList<String> passengerList) {
-        this.passengerList = passengerList;
-    }
-
-    public HashMap<String, String> getStopsList() {
-        return stopsList;
-    }
-
-    public void setStopsList(HashMap<String, String> stopsList) {
-        this.stopsList = stopsList;
-    }
-
-    public void addListPassenger(String s) {
-        this.passengerList.add(s);
     }
 }
